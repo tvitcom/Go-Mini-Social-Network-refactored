cf43a63dd8e817cedc0d75bc12869f4ed66504aa
r <you@example.com>
2020-06-17 01:37:43 +0300

    add dbhost param from _env file

--- config/db.go
+++ config/db.go
@@ -11,11 +11,12 @@ import (
 // DB function
 func DB() *sql.DB {
 
+	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	_db := os.Getenv("DB")
 
-	db, _ := sql.Open("mysql", user+":"+password+"@tcp(127.0.0.1:3306)/"+_db)
+	db, _ := sql.Open("mysql", user+":"+password+"@tcp("+host+":3306)/"+_db)
 	err := db.Ping()
 	if err != nil {
 		panic(err)
